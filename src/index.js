a3b5cf57ec1134f939750e79e55726e48e2a389b
sankoff <sankoff84@gmail.com>
2023-01-13 12:09:03 +0300

    write code index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let num = Math.abs(n);
+    
+    let rev_num = 0;
+    while(num > 0)
+    {
+        rev_num = rev_num * 10 + num % 10;
+        num = Math.floor(num / 10);
+    }
+    return rev_num;
 }
